9bebf68bd31e5a7955bae9dd0558c29c8fee8d53
Frankpeter Ani <frankpeterani@gmail.com>
2021-01-04 14:18:21 +0100

    First page looking good

--- js/body-animation.js
+++ js/body-animation.js
@@ -1,26 +1,34 @@
 // banner area
 gsap.to('.slider1', {duration: 1, x: '-100%', delay: .2, ease: 'power2.in'});
 gsap.to('.slider2', {duration: 1, x: '100%', delay: .2, ease: 'power2.in'});
-gsap.from('.logo, .quick_links, .quick_social_links, .menu_icon', {duration: .5, opacity: 0.3, delay: 1.2, ease: 'bounce'});
+gsap.from('.logo, .quick_links, .quick_social_links, .menu_icon', {duration: .5, opacity: 0.3, delay: 1.2, ease: 'elastic'});
 gsap.from('.banner_text h3', {duration: 1, opacity: 0, delay: 1.5, y: '-100%', scale: 2});
 gsap.from('.branding_interaction h4', {duration: 1, opacity: 0, delay: 1.7});
 gsap.from('.branding_interaction p', {duration: 1, opacity: 0, delay: 2, scale: 1.5});
 gsap.from('.banner_text a', {duration: 1, opacity: 0, delay: 2.2,});
 
+// gsap.register(ScrollTrigger);
+// gsap.from(
+//    ".section_title", {
+//       duration: 3, x: "-50vw", ease: "linear",
+//       ScrollTrigger:{
+//          trigger: ".section_title",
+//          start: "20px 80%",
+//          end: "bottom 100px",
+//          markers: true,
+//          toggleActions: "restart pause resume none"
+//       }
+//    }
+// )
 
-// second sections using scrollTrigger Plugin
 gsap.register(ScrollTrigger);
-gsap.to(
-   ".service_area .section_title", {
+
+gsap.from(
+   '.service_area .section_title', {
       ScrollTrigger:{
-         trigger: ".service_area .section_title",
-         start: "20px 80%",
-         end: "bottom 100px",
+         trigger: '.section_title',
+         start: 'top center',
          markers: true,
-         toggleActions: "restart pause resume none"
-      },
-      x: 400,
-      rotation: 360,
-      duration: 3
+      }
    }
 )
